e743516413c09d13496ad79b42b55e92126f798d
William Bryan <wjbvii@gmail.com>
2023-05-06 12:30:20 -0500

    add textwidth=80 and colorcolumn={81}

---
+++ dap.lua
@@ -0,0 +1,19 @@
+return {
+  "mfussenegger/nvim-dap",
+  -- enabled = vim.fn.has "win32" == 0,
+  enabled = true,
+  dependencies = {
+    {
+      "jay-babu/mason-nvim-dap.nvim",
+      dependencies = { "nvim-dap" },
+      cmd = { "DapInstall", "DapUninstall" },
+      opts = { handlers = {} },
+    },
+    {
+      "rcarriga/nvim-dap-ui",
+      opts = { floating = { border = "rounded" } },
+      config = require "plugins.configs.nvim-dap-ui",
+    },
+  },
+  event = "User AstroFile",
+}
